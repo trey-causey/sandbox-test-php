c28db53205c544e79b512efc42b9f2d83c17702e
trey-causey <trey.causey@outlook.com>
2020-10-28 21:43:28 -0400

    added MyCommand pattern test

--- tests/design_patterns/behavioral/TestCommand.php
+++ tests/design_patterns/behavioral/TestCommand.php
@@ -2,6 +2,8 @@
 
 namespace design_patterns\behavioral\command\tests;
 
+include __DIR__ . '/../../../src/design_patterns/behavioral/command/commandIndex.php';
+
 use design_patterns\behavioral\command\concreteCommandA;
 use design_patterns\behavioral\command\Invoker;
 use design_patterns\behavioral\command\Receiver;
@@ -10,14 +12,12 @@ use design_patterns\behavioral\command;
 
 class TestCommand extends TestCase
 {
+    /** @test */
     public function TestInvocation()
     {
-    $testInvokerObject = new Invoker();
-    $testReceiverObject = new Receiver();
-    $testInvokerObject->setCommand(new concreteCommandA($testReceiverObject));
-    $testInvokerObject->run();
-
-
+        $testInvokerObject = new Invoker();
+        $testReceiverObject = new Receiver();
+        $testInvokerObject->setCommand(new concreteCommandA($testReceiverObject));
+        $testInvokerObject->run();
     }
-
 }
