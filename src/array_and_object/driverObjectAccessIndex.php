df299739d81ebff496887b6706b3b580cc1f4d7b
trey-causey <trey.causey@outlook.com>
2020-11-08 08:58:33 -0500

    added object testing to adapt finished picks to driver table, tested and working

---
+++ src/array_and_object/driverObjectAccessIndex.php
@@ -0,0 +1,37 @@
+<?php
+
+$cache_file = __DIR__ . '/../../resources/files/drivers.json';
+$pickFile = __DIR__ . '/../../resources/files/draftsFromSpreadsheet.json';
+
+$testData = file_get_contents($cache_file);
+$pickFileContents = file_get_contents($pickFile);
+
+$testOutput = json_decode($testData);
+$decodedJSONfromPickFile = json_decode($pickFileContents);
+$driverArray = array();
+foreach (($testOutput[2]->data) as $driverObjects) {
+    array_push($driverArray, $driverObjects);
+}
+$driverCodeMap = array();
+foreach ($driverArray as $item) {
+    if($item->code != NULL) {
+        $driverCodeMap[$item->driverRef] = $item->code;
+    }
+
+}
+$pickArray = array();
+
+foreach ($decodedJSONfromPickFile->data as $item)
+{
+    array_push($pickArray, $item);
+}
+
+foreach ($pickArray as $i) {
+    $owner = $i->Owner;
+   $ownerDriverArray = [$i->Driver1, $i->Driver2, $i->TurboDriver ];
+    foreach ($ownerDriverArray as $name) {
+        $name = strtolower($name);
+        echo $driverCodeMap[$name] . "\n";
+    }
+}
+
